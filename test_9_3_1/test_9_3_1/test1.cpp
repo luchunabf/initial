2a257a4c9eba812777f81cd84f2ffd08d30a5474
PNUHC <luchunabf@163.com>
2019-09-03 18:32:35 +0800

    String-PNUHC

---
+++ test_9_3_1/test_9_3_1/test1.cpp
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <assert.h>
+//using namespace std;
+
+class String
+{
+public:
+	String(const char* str = "")
+	{
+		if (nullptr == str)
+		{
+			return;
+		}
+		_str = new char[strlen(str) + 1];
+		strcpy(_str, str);
+	}
+	String(const String& s)
+		:_str(new char[strlen(s._str) + 1])
+	{
+		strcpy(_str, s._str);
+	}
+	String& operator= (const String& s)
+	{
+		if (this != &s)
+		{
+			char* pStr = new char[strlen(s._str) + 1];
+			strcpy(pStr, s._str);
+			delete[] _str;
+			_str = pStr;
+		}
+		return *this;
+	}
+	~String()
+	{
+		if (_str)
+		{
+			delete[] _str;
+			_str = nullptr;
+		}
+	}
+private:
+	char* _str;
+};
+int main()
+{
+	String s("hello world");
+	//cout << s << endl;
+	
+}
