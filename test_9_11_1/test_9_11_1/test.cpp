5bb32f3690c7e2c0c358b9a1d84d06f893cf1d27
PNUHC <luchunabf@163.com>
2019-09-11 16:07:37 +0800

    strlen

--- test_9_11_1/test_9_11_1/test.cpp
+++ test_9_11_1/test_9_11_1/test.cpp
@@ -1,10 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <iostream>
 #include <string>
+#include <assert.h>
 using namespace std;
 
-int MyStrlen(char* str)
+int MyStrlen1(char* str)
 {
+	if (nullptr == str)
+	{
+		assert(0);
+		return -1;
+	}
 	int count = 0;
 	while (*str)
 	{
@@ -14,9 +20,28 @@ int MyStrlen(char* str)
 	return count;
 }
 
+
+int MyStrlen2(char* str)
+{
+	if (nullptr == str)
+	{
+		assert(0);
+		return -1;
+	}
+	char* p = str;
+	while (*p)
+	{
+		
+		++p;
+	}
+	return p-str;
+}
+
 int main()
 {
 	char* str = "abc";
-	cout << MyStrlen(str);
+	cout << MyStrlen1(str);
+	cout << MyStrlen2(str);
+
 	return 0;
 }
