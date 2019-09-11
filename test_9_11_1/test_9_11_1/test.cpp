0a572a0b53e53bd2ac4fdd792d4733522b998aaf
PNUHC <luchunabf@163.com>
2019-09-11 16:09:39 +0800

    strlen-PNUHC

--- test_9_11_1/test_9_11_1/test.cpp
+++ test_9_11_1/test_9_11_1/test.cpp
@@ -37,11 +37,24 @@ int MyStrlen2(char* str)
 	return p-str;
 }
 
+int MyStrlen3(char* str)
+{
+	if (nullptr == str)
+	{
+		assert(0);
+		return -1;
+	}
+	if (*str == '\0')
+		return 0;
+	return 1 + MyStrlen3(str + 1);
+}
+
 int main()
 {
 	char* str = "abc";
-	cout << MyStrlen1(str);
-	cout << MyStrlen2(str);
+	cout << MyStrlen1(str) << endl;
+	cout << MyStrlen2(str)<< endl;
+	cout << MyStrlen3(str)<< endl;
 
 	return 0;
 }
