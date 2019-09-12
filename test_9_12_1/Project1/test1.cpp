5a2f3258742076c1927b24ef11a57205b905643d
PNUHC <luchunabf@163.com>
2019-09-12 23:48:51 +0800

    memcpyTest

--- test_9_12_1/Project1/test1.cpp
+++ test_9_12_1/Project1/test1.cpp
@@ -1,12 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <string.h>
 #include <iostream>
+#include <assert.h>
 using namespace std;
+
+void* my_memcpy(void* dst, const void* src, int num)
+{
+	assert(dst != nullptr && src != nullptr);
+	void* ret = dst;
+	while (num--)
+	{
+		(*(char*)dst)++ = (*(char*)src)++;
+	}
+	return ret;
+}
+
+
 
 int main()
 {
 	char str1[] = "abcd";
 	char str2[] = "abcg";
-	cout << memcmp(str1, str2, 4) << endl;
+	//cout << memcmp(str1, str2, 4) << endl;
+
 	return 0;
 }
