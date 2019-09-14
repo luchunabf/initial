12061521efcd67848292460d07d2d06e58c30ebd
PNUHC <luchunabf@163.com>
2019-09-14 23:39:45 +0800

    testIO-PNUHC

--- test_9_14_1/test_9_14_1/test.cpp
+++ test_9_14_1/test_9_14_1/test.cpp
@@ -7,11 +7,15 @@ int main()
 	int N, M;
 	while (cin >> N >> M)
 	{
+		if (0 == N || 0 == M)
+			continue;
+		//int arr[M] 
 		for (int i = 0; i < M; ++i)
 		{
 			int a = 0, b=0, c=0;
 			cin >> a >> b >> c;
-			cout << a <<" " <<  b  << " " << c << endl;
+			//cout << a <<" " <<  b  << " " << c << endl;
+
 		}
 	}
 	return 0;
