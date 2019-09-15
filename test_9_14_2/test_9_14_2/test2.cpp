7ff254b8dab1ea24b1b8750ebfc5ff91b11ac1ee
PNUHC <luchunabf@163.com>
2019-09-15 11:10:30 +0800

    mapTest-PNUHC

--- test_9_14_2/test_9_14_2/test2.cpp
+++ test_9_14_2/test_9_14_2/test2.cpp
@@ -3,8 +3,10 @@
 #include <map>
 #include <string>
 #include <set>
+#include <vector>
 using namespace std;
 
+#if 0
 int main()
 {
 	map<string, string> m;
@@ -15,7 +17,7 @@ int main()
 	auto it = m.begin();
 	while (it != m.end())
 	{
-		cout << it->first << "-->" << it->second << endl;
+		cout << it->first << "-->" << it->second << endl;///////////////
 		++it;
 	}
 
@@ -34,5 +36,53 @@ int main()
 	{
 	cout << e.first;
 	}*/
+	return 0;
+}
+#endif
+
+int main()
+{
+	//set<string> s;
+	/*s.insert("apple");
+	s.insert("peach");
+	s.insert("peer");
+
+	auto it = s.begin();
+	while (it != s.end())
+	{
+	cout << *it << endl;
+	++it;
+	}
+
+	s.erase("peer");
+
+	for (auto e : s)
+	{
+	cout << e << endl;
+	}*/
+	/*set<int> s;
+	vector<int> v{ 5, 3, 4, 7, 9, 6, 1, 2, 8, 0 };
+	for (int i = 0; i < v.size(); ++i)
+	{
+		s.insert(v[i]);
+	}
+
+	auto it = s.begin();
+	while (it != s.end())
+	{
+		cout << *it << " ";
+		++it;
+	}
+	cout << endl;*/
+
+	vector<int> v{ 5, 3, 4, 7, 9, 6, 1, 2, 8, 0 };
+	set<int> s(v.begin(), v.end());
+	auto it = s.begin();
+	while (it != s.end())
+	{
+		cout << *it << " ";
+		++it;
+	}
+	cout << endl;
 	return 0;
 }
