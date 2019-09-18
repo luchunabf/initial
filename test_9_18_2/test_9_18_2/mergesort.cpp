8492b4b351aca17d5644e597b914648b319e1739
PNUHC <luchunabf@163.com>
2019-09-18 23:46:48 +0800

    MergeSort-PNUHC

--- test_9_18_2/test_9_18_2/mergesort.cpp
+++ test_9_18_2/test_9_18_2/mergesort.cpp
@@ -9,7 +9,8 @@ void MergeData(int* arr, int left, int mid, int right, int* temp)
 {
 	int begin1 = left, end1 = mid;
 	int begin2 = mid, end2 = right;
-	int index = 0;
+	//int index = 0;/////////////////自己错误之处，不能写成0，写0则写死了，导致不能递归
+	int index = left;
 	while (begin1 < end1 && begin2 < end2)
 	{
 		if (arr[begin1] < arr[begin2])
@@ -25,11 +26,14 @@ void MergeData(int* arr, int left, int mid, int right, int* temp)
 
 void _MergeSort(int* arr, int left, int right, int* temp)
 {
-	int mid = left + ((right - left) >> 1);
-	_MergeSort(arr, left, mid, temp);
-	_MergeSort(arr, mid, right, temp);
-	MergeData(arr, left, mid, right, temp);
-	memcpy(arr + left, temp + left, sizeof(int)*(right - left));
+	if (right - left > 1)
+	{
+		int mid = left + ((right - left) >> 1);
+		_MergeSort(arr, left, mid, temp);
+		_MergeSort(arr, mid, right, temp);
+		MergeData(arr, left, mid, right, temp);
+		memcpy(arr + left, temp + left, sizeof(int)*(right - left));
+	}
 }
 
 void MergeSort(int* arr, int size)
@@ -49,6 +53,8 @@ int main()
 {
 	int arr[] = { 5, 4, 7, 9, 8, 1, 3, 0, 2, 6 };
 	MergeSort(arr, 10);
-	cout << arr << endl;
+	for (auto e : arr)
+		cout << e << " ";
+	cout << endl;
 	return 0;
 }
