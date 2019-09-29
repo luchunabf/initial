941251d5b7a19641476b75e896af1c1433db6f57
PNUHC <luchunabf@163.com>
2019-09-29 18:37:38 +0800

    virtual1-PNUHC

---
+++ test_9_29_2/test_9_29_2/test.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+
+class Person
+{
+public:
+	virtual void F1() = 0;
+	virtual void F2() = 0;
+	virtual void F3() = 0;
+};
+
+class Student : public Person
+{
+public:
+	void F1()
+	{
+		cout << "hello" << endl;
+	}
+	void F2()
+	{
+		cout << "world" << endl;
+	}
+};
+
+int main()
+{
+	//Person p;
+	Student s;
+	s.F1();
+	s.F2();
+	s.F3();
+	return 0;
+}
