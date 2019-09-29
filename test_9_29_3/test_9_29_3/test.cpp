e75ba2cc194dd00c1792749e0c87cbec9ffa01ba
PNUHC <luchunabf@163.com>
2019-09-29 21:02:41 +0800

    大小端-PNUHC

---
+++ test_9_29_3/test_9_29_3/test.cpp
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+
+int CheckSys()
+{
+	int i = 1;
+	if (*(char*)&i)
+		return 1;
+	else
+		return 0;
+}
+
+
+int check_sys()
+{
+	union Un
+	{
+		int i;
+		char c;
+	}un;
+	un.i = 1;
+	return un.c;
+
+}
+int main()
+{
+	if (CheckSys())
+		cout << "小端" << endl;
+	else
+		cout << "大端" << endl;
+
+	return 0;
+}
+
+
