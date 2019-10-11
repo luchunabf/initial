6682de0eda8380cfd5b28b66314d9c2a1c485fa1
PNUHC <luchunabf@163.com>
2019-10-11 20:59:31 +0800

    test-PNUHC

---
+++ test_10_10_1/test_10_10_1/test1.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 0^0;
+	int b = 0 ^ 2;
+	cout << a << endl;
+	cout << b << endl;
+	return 0;
+}
